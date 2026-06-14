b9e4f384eadf4bfb9d98601966cbe450aa5a1f51
rachelyqzhao-ai <rachelyqzhao@gmail.com>
2026-06-14 18:25:48 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app.tsx'
+import App from './app.tsx' // Make sure this is lowercase 'a'!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
